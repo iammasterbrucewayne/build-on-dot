ba924290fc7774757392b608d92c979af4e426fb
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2024-03-23 14:24:09 +0530

    add mobile view for NavLinksComponent

--- src/app/_components/navbar/NavLinksComponent.tsx
+++ src/app/_components/navbar/NavLinksComponent.tsx
@@ -1,17 +1,37 @@
 import { map } from "lodash";
 
 import { NavLinks } from "@/app/_lib/constants/nav-links";
-import { HStack } from "@chakra-ui/react";
+import { HStack, VStack } from "@chakra-ui/react";
 import { Link } from "@chakra-ui/next-js";
 
-export default function NavLinksComponent() {
-  return (
-    <HStack spacing={4}>
-      {map(NavLinks, (link) => (
-        <Link href={link.href} key={link.title} fontWeight="semibold">
-          {link.title}
-        </Link>
-      ))}
+export default function NavLinksComponent({
+  isMobile,
+}: {
+  isMobile?: boolean;
+}) {
+  const links = map(NavLinks, (link) => (
+    <Link
+      href={link.href}
+      key={link.title}
+      fontWeight="semibold"
+      py={isMobile ? 3 : "initial"}
+    >
+      {link.title}
+    </Link>
+  ));
+  return isMobile ? (
+    <VStack spacing={0} my={4}>
+      {links}
+    </VStack>
+  ) : (
+    <HStack
+      spacing={4}
+      display={{
+        base: "none",
+        md: "flex",
+      }}
+    >
+      {links}
     </HStack>
   );
 }
