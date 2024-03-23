d4b5a95a0976350ac9c3411bdbb5e4228c7df150
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2024-03-23 19:36:26 +0530

    add link to homepage on navbar brand

--- src/app/_components/common/navbar/NavbarBrand.tsx
+++ src/app/_components/common/navbar/NavbarBrand.tsx
@@ -1,9 +1,16 @@
-import { Image } from "@chakra-ui/next-js";
+import { Image, Link } from "@chakra-ui/next-js";
 import { HStack, Text } from "@chakra-ui/react";
 
 export default function NavbarBrand() {
   return (
-    <HStack spacing={4}>
+    <HStack
+      as={Link}
+      href="/"
+      spacing={4}
+      _hover={{
+        textDecoration: "none",
+      }}
+    >
       <Image
         src="/images/android-chrome-192x192.png"
         alt="Build On Dot"
