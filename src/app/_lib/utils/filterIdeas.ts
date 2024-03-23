1daf7cd1f9279b493607a495b397f7218c48a774
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2024-03-23 19:34:15 +0530

    extract filterIdeas as a util function into a separate file with search filter addition

---
+++ src/app/_lib/utils/filterIdeas.ts
@@ -0,0 +1,45 @@
+import { Category } from "../constants/ideas/category";
+import { Idea } from "../types/ideas";
+import { filter, isNil } from "lodash";
+
+export const filterIdeas = (
+  ideas: Idea[],
+  category: Category,
+  query: string | null
+) => {
+  const lowercaseCategory = category?.toLowerCase();
+  const lowercaseQuery = query?.toLowerCase() || null;
+
+  return filter(
+    ideas,
+    (idea) =>
+      (idea.category.toLowerCase() === lowercaseCategory ||
+        !lowercaseCategory ||
+        lowercaseCategory === "all") &&
+      matchesQuery(toLowerCaseIdea(idea), lowercaseQuery)
+  );
+};
+
+const matchesQuery = (idea: Idea, query: string | null) => {
+  return (
+    !query ||
+    query === "" ||
+    idea.title.includes(query) ||
+    idea.description.problem.includes(query) ||
+    idea.description.solution.includes(query) ||
+    idea.author.name.includes(query)
+  );
+};
+
+const toLowerCaseIdea = (idea: Idea) => ({
+  slug: idea.slug,
+  title: idea.title.toLowerCase(),
+  author: { ...idea.author, name: idea.author.name.toLowerCase() },
+  description: {
+    problem: idea.description.problem.toLowerCase(),
+    solution: idea.description.solution.toLowerCase(),
+    useful_resources: idea.description.useful_resources,
+  },
+  category: idea.category, // Don't lowercase category
+  difficulty: idea.difficulty, // Don't lowercase difficulty
+});
--- src/app/ideas/page.tsx
+++ src/app/ideas/page.tsx
@@ -3,14 +3,17 @@
 import { Box, Grid } from "@chakra-ui/react";
 import Navbar from "@/app/_components/common/navbar";
 import { map, filter } from "lodash";
-import { ideas } from "../_lib/constants/placeholderIdeas";
-import IdeaOverviewCard from "../_components/ideas/IdeaOverviewCard";
-import { Idea } from "../_lib/types/ideas";
+import { ideas } from "@/app/_lib/constants/placeholderIdeas";
+import IdeaOverviewCard from "@/app/_components/ideas/IdeaOverviewCard";
+import { Idea } from "@/app/_lib/types/ideas";
 import { useSearchParams } from "next/navigation";
+import { filterIdeas } from "@/app/_lib/utils/filterIdeas";
+import { Category } from "@/app/_lib/constants/ideas/category";
 
 export default function Ideas() {
   const search = useSearchParams();
   const category = search.get("category");
+  const query = search.get("query");
   return (
     <Box as="main" p={4}>
       <Navbar />
@@ -18,16 +21,9 @@ export default function Ideas() {
         templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)"]}
         gap={4}
       >
-        {map(
-          filter(
-            ideas,
-            (idea) =>
-              idea.category === category || !category || category === "all"
-          ),
-          (idea: Idea) => (
-            <IdeaOverviewCard key={idea.slug} idea={idea} />
-          )
-        )}
+        {map(filterIdeas(ideas, category as Category, query), (idea: Idea) => (
+          <IdeaOverviewCard key={idea.slug} idea={idea} />
+        ))}
       </Grid>
     </Box>
   );
