31c51ae672a32f94271edd96a4ec51465a03d99f
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2024-03-24 04:39:09 +0530

    update hero description

--- src/app/_components/home/hero/HeroDescription.tsx
+++ src/app/_components/home/hero/HeroDescription.tsx
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react";
 export default function HeroDescription() {
   return (
     <Text mt={8} fontSize="xl">
-      We&apos;ve curated a list of diverse project ideas that are perfect for
+      We&apos;ve curated a list of diverse project ideas, perfect for kickstarting
       your web3 journey. Whether you&apos;re a beginner or an expert,
       there&apos;s something for everyone.
     </Text>
