b6d1c4ed89fe19f61af5d4cc94b238f3d64ac547
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2024-03-23 14:23:52 +0530

    add mobile view for navbar actions

--- src/app/_components/navbar/NavbarActions.tsx
+++ src/app/_components/navbar/NavbarActions.tsx
@@ -1,9 +1,39 @@
-import { Button, HStack, IconButton } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  VStack,
+  IconButton,
+  InputGroup,
+  Input,
+  InputLeftElement,
+} from "@chakra-ui/react";
 import { FiSearch } from "react-icons/fi";
 
-export default function NavbarActions() {
-  return (
-    <HStack spacing={4}>
+export default function NavbarActions({ isMobile }: { isMobile?: boolean }) {
+  return isMobile ? (
+    <VStack spacing={4}>
+      <InputGroup>
+        <InputLeftElement pointerEvents="none">
+          <FiSearch />
+        </InputLeftElement>
+        <Input
+          placeholder="Search for an idea"
+          rounded="full"
+          borderColor="gray.600"
+        />
+      </InputGroup>
+      <Button rounded="full" w="full">
+        Submit an idea
+      </Button>
+    </VStack>
+  ) : (
+    <HStack
+      spacing={4}
+      display={{
+        base: "none",
+        md: "flex",
+      }}
+    >
       <IconButton
         aria-label="Search for an idea"
         icon={<FiSearch />}
