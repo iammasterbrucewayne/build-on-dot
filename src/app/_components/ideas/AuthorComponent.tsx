b632870b3996ca6c4ae96db79fe4dbc85ff9b685
iammasterbrucewayne <iammasterbrucewayne@protonmail.com>
2024-03-24 04:22:27 +0530

    basic layout for idea page

--- src/app/_components/ideas/AuthorComponent.tsx
+++ src/app/_components/ideas/AuthorComponent.tsx
@@ -1,11 +1,22 @@
 import { Avatar, HStack, Text } from "@chakra-ui/react";
 import { Author } from "../../_lib/types/ideas";
 
-export default function AuthorComponent({ author }: { author: Author }) {
+export default function AuthorComponent({
+  author,
+  isFull,
+}: {
+  author: Author;
+  isFull?: boolean;
+}) {
   return (
     <HStack>
       <Avatar src={author.profilePicture} size="sm" />
-      <Text fontWeight="semibold">{author.name}</Text>
+      <Text
+        fontWeight={isFull ? "800" : "semibold"}
+        fontSize={isFull ? "lg" : "md"}
+      >
+        {author.name}
+      </Text>
     </HStack>
   );
 }
--- src/app/ideas/[slug]/page.tsx
+++ src/app/ideas/[slug]/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import Navbar from "@/app/_components/common/navbar";
+import AuthorComponent from "@/app/_components/ideas/AuthorComponent";
 import { ideas } from "@/app/_lib/constants/placeholderIdeas";
 import { Idea } from "@/app/_lib/types/ideas";
+import { Link } from "@chakra-ui/next-js";
 import {
   Box,
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  Heading,
+  ListItem,
+  Stack,
+  Text,
+  UnorderedList,
 } from "@chakra-ui/react";
 import { redirect } from "next/navigation";
 
@@ -18,18 +27,58 @@ export default function IdeaPage({ params }: { params: { slug: string } }) {
   const { title, description, author, category, slug }: Idea = idea;
 
   return (
-    <Box as="main" p={4}>
+    <Box as="main" p={4} maxW="6xl" mx="auto">
       <Navbar />
-      <Breadcrumb separator=">">
-        <BreadcrumbItem>
-          <BreadcrumbLink href={`/ideas/?category=${category}`}>
-            {category}
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbItem isCurrentPage>
-          <BreadcrumbLink href={`/ideas/${slug}`}>{title}</BreadcrumbLink>
-        </BreadcrumbItem>
-      </Breadcrumb>
+      <Box p={4}>
+        <Breadcrumb separator=">">
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/ideas/?category=All">All</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href={`/ideas/?category=${category}`}>
+              {category}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem isCurrentPage>
+            <BreadcrumbLink href={`/ideas/${slug}`}>{title}</BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+        <Stack spacing={12} mt={8} color="white">
+          <Stack spacing={6}>
+            <Heading as="h1" size="xl" fontWeight="black">
+              {title}
+            </Heading>
+            <AuthorComponent author={author} isFull />
+          </Stack>
+          <Stack spacing={6}>
+            <Heading as="h2" size="lg">
+              Problem to solve
+            </Heading>
+            <Text>{description.problem}</Text>
+          </Stack>
+          <Stack spacing={6}>
+            <Heading as="h2" size="lg">
+              Possible Solution
+            </Heading>
+            <Text>{description.problem}</Text>
+          </Stack>
+          <Stack spacing={6}>
+            <Heading as="h2" size="lg">
+              Useful resources
+            </Heading>
+
+            <UnorderedList spacing={2}>
+              {description.useful_resources.map((resource, index) => (
+                <ListItem key={index}>
+                  <Link href={resource.url} color="blue.200" isExternal>
+                    {resource.name}
+                  </Link>
+                </ListItem>
+              ))}
+            </UnorderedList>
+          </Stack>
+        </Stack>
+      </Box>
     </Box>
   );
 }
